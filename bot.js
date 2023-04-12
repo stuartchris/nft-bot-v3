95177f368062b17ace0fabd26fd7a33ac6342e78
stuartchris <chriskstuart@gmail.com>
2023-04-12 07:58:10 -0700

    removed 10x gas

--- bot.js
+++ bot.js
@@ -396,8 +396,8 @@ async function startFetchingLatestGasPrices() {
 				// TODO: Add support for EIP1159 provider fetching here
 			} else if (NETWORK.gasMode === GAS_MODE.LEGACY) {
 				let gasPrice = await currentlySelectedWeb3Client.eth.getGasPrice();
-				gasPrice = gasPrice * 10
-				console.log(`gas price: ${gasPrice}`)
+				// gasPrice = gasPrice * 10
+				// console.log(`gas price: ${gasPrice}`)
 				// const gasPrice = process.env.GAS_PRICE;
 				gasPriceBn = new Web3.utils.BN(gasPrice);
 			}
